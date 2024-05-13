7f1215d0a9642d1ecd8e3af9afcd0d1a0b83a797
kianakermani <kianakermani2001@gmail.com>
2024-05-13 04:55:46 -0700

    made seprate file for theme

--- methods/themeSelector.js
+++ methods/themeSelector.js
@@ -1,20 +1,15 @@
 $(document).ready(function () {
-  // Add options to the theme selector
   $("#theme-selector").append("<option value='light'>روشن</option>");
   $("#theme-selector").append("<option value='dark'>تیره</option>");
 
-  // Set the default theme
   $("#theme-selector").val("light");
 
-  // Handle theme change
   $("#theme-selector").change(function () {
     var theme = $(this).val();
-
-    // Toggle classes based on the selected theme
-    if (theme === "light") {
-      $("html").removeClass("dark-theme");
+    if (theme === "dark") {
+      $("#stylesheet").attr("href", "./styles/dark.css");
     } else {
-      $("html").addClass("dark-theme");
+      $("#stylesheet").attr("href", "./styles/light.css");
     }
   });
 });
